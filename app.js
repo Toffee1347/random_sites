64f29e533b1021fa48ee5f4667e43769edabc194
Daniel Howard <danielnhoward93@gmail.com>
2020-08-14 14:21:01 +0100

    Added baseline for game

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var fs = require('fs')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+app.get('*', (req, res) => {
+    let file
+    let url = req.url.split('?')
+    if (url[0].endsWith('/index.html')) {
+        file = __dirname + '/public' + url[0]
+    } 
+    else if (url[0].endsWith('/index.html/')) {
+        file = __dirname + '/public' + url[0].substring(0, url[0].length - 1)
+    } 
+    else {
+        file = __dirname + '/public' + url[0] + '/index.html'
+    }
+    fs.readFile(file, function(error, data) {
+        if (error) {
+            res.statusCode = 404
+            res.sendFile(__dirname + '/public/404error.html', 404);
+        }
+        else {
+            res.statusCode = 200
+            res.sendFile(file);
+        }
+    })
+  });
+
+io.on('connection', (socket) => {
+    socket.on('ttt_start', (data) => {
+      console.log(data.game_id);
+    });
+  });
+
+http.listen(80, () => {
+  console.log('listening on *:3000');
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+function print(message) {
+    console.log(message)
+}
